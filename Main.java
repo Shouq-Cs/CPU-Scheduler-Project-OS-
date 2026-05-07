522f2da42cba5fa6200a7d65afcb0ab920f060cf
AfnanSaad <445200403@student.ksu.edu.sa>
2026-05-07 03:03:44 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,53 +17,8 @@ public class Main {
         results.add(new ScheduleResult(processes.get(3), 58, 68));
 
         // Print scheduler results
-        printResults(results);
+      OutputManager.printResults(results);
     }
 
-    // Display Gantt chart and scheduling statistics
-    public static void printResults(ArrayList<ScheduleResult> results) {
-
-        // Print Gantt Chart
-        System.out.println("\nGantt Chart:");
-
-        for (ScheduleResult r : results) {
-            System.out.print("| P" + r.process.id + " ");
-        }
-
-        System.out.println("|");
-
-        for (ScheduleResult r : results) {
-            System.out.print(r.startTime + "    ");
-        }
-
-        System.out.println(results.get(results.size() - 1).finishTime);
-
-        // Print results table
-        System.out.println("\nResults Table:");
-
-        System.out.println("-------------------------------------------------------------");
-
-        System.out.printf("%-10s %-10s %-10s %-10s %-15s%n",
-                "Process", "Start", "Finish", "Waiting", "Turnaround");
-
-        System.out.println("-------------------------------------------------------------");
-
-        // Print each process result
-        for (ScheduleResult r : results) {
-
-            // Calculate waiting and turnaround time
-            int waiting = r.startTime;
-
-            int turnaround = r.finishTime;
-
-            System.out.printf("%-10s %-10d %-10d %-10d %-15d%n",
-                    "P" + r.process.id,
-                    r.startTime,
-                    r.finishTime,
-                    waiting,
-                    turnaround);
-        }
-
-        System.out.println("-------------------------------------------------------------");
-    }
+   
 }
