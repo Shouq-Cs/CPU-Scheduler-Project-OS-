17e351c4a04c57a5e6fb4acbc0d4a04f5c00724c
Saharvx <sahaar5002@gmail.com>
2026-05-09 14:01:59 +0300

    SystemManager.java

--- SystemManager.java
+++ SystemManager.java
@@ -3,9 +3,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 public class SystemManager {
 	
-	    public static final Queue<Process> jobQueue = new LinkedList<>();
-	    public static final Queue<Process> readyQueue = new LinkedList<>();
-	    public static int currentMemoryUsed = 0;
-	    public static final int MAX_MEMORY = 2048;
-	    public static final Object lock = new Object();
-	}
+    public static final Queue<Process> jobQueue = new LinkedList<>();
+    public static final Queue<Process> readyQueue = new LinkedList<>();
+    public static int currentMemoryUsed = 0;
+    public static final int MAX_MEMORY = 2048;
+    public static int globalTime=0;
+    public static final Object lock = new Object();
+}
